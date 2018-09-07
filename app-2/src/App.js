be3b3c6f917ff9493826a0794a212d66c2566e52
Eric Vere <erkver250@gmail.com>
2018-09-07 14:29:03 -0500

    Completed App-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      food: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    }
+  }
+
   render() {
+    let foodCopy = this.state.food.map((e, i) => {
+      return(
+        <h2 key={ i }>{ e }</h2>
+      );
+    });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { foodCopy }
       </div>
     );
   }
