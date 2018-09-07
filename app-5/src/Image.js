818daeece257e82d19a734c7cf8ad8abc1b96106
Eric Vere <erkver250@gmail.com>
2018-09-07 17:28:03 -0500

    Completed app-5

---
+++ app-5/src/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Image = (props) => {
+    return (
+        <div>
+            <img src={props.myImage} alt="" />
+        </div>
+    );
+}
+
+
+export default Image;
