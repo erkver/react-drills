f67d1f53bb0415b5dc9928e98eaf00822576f24c
Eric Vere <erkver250@gmail.com>
2018-09-07 15:37:43 -0500

    Finished App-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      food: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"],
+      userInput: ""
+    }
+  }
+
+  filterFood = (str) => {
+    this.setState({userInput: str.target.value});
+  }
+
   render() {
+    console.log(this.state.userInput);
+    let foodCopy = this.state.food
+    .filter(foods => foods.includes(this.state.userInput))
+    .map((e, i) => {
+      return(
+        <h2 key={ i }>{ e }</h2>
+      );
+    });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={(e) => this.filterFood(e)}></input>
+        { foodCopy }
       </div>
     );
   }
